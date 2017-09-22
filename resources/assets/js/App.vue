58de9c4b09fee0d640c82c1ddd524cda227cc1f1
Nabil Baadillah <baadillahnabil@gmail.com>
2017-09-23 02:13:42 +0700

    Layouting

---
+++ resources/assets/js/App.vue
@@ -0,0 +1,20 @@
+<template lang = "pug">
+    .component-root $END$
+</template>
+
+<script>
+    export default {
+        name: '$COMPONENTNAME$',
+        data() { return {} }
+    }
+</script>
+
+<!-- for styling with stylus -->
+<style lang = "stylus">
+
+</style>
+
+<!-- for styling with sass / scss -->
+<style lang = "sass">
+
+</style>
